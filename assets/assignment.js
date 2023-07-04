a3885fc4c2eb79d7ea1bef323f0f970a773acdec
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-04 16:55:43 -0400

    worked assignment

---
+++ assets/assignment.js
@@ -0,0 +1,78 @@
+class Course {
+
+    constructor(title, length, price) {
+        this.title = title;
+        this.length = length;
+        this.price = price
+    }
+
+    get calc(){
+        const sum = this.length / this.price
+        return `I'll get ${sum} credits and will take ${this.length} days.`
+    }
+
+    set calc(val){
+        if(this.price < 0) {
+            console.log('Bertha wnats to help!')
+            console.log(`You don't have enough to take ${this.title} this semester` )
+            this.price = 5            
+            return
+
+        }
+    }
+    
+    summary() {
+        return `You choose ${this.title}. It last ${this.length} days and cost \$${this.price} Simon.`
+    }
+}
+
+const offerings = {
+    choices: [
+        new Course('Math', 100, 10),
+        new Course('Science', 120, 30)
+    ],
+
+    render() {
+        for(const choice of this.choices) {
+            console.log(choice.calc)
+            console.log(choice.summary())
+        }
+    }
+}
+
+class PracticalCourse extends Course {
+    constructor(title, length, price, numOfExercises, name) {
+        super(title, length, price)
+        this.numOfExercises = numOfExercises
+        this.name = name
+    }
+    render(){
+        console.log(`${this.name}, there will be ${this.numOfExercises} assignments. ${this.calc}`)
+
+    }
+}
+
+class TheoreticalCourse extends Course {
+    constructor(title, length, price, name) {
+        super(title, length, price,)
+        this.name = name
+    }
+    publish() {
+        console.log(`${this.name}, Booker T Washington said ${this.title} is the way forward. ${this.calc}.`)
+    }
+}
+
+
+// offerings.render()
+const practical = new PracticalCourse('English',40, 2, 30, 'Bertha')
+practical.calc = this.price
+practical.render()
+
+
+
+
+const theoretical = new TheoreticalCourse('Agriculture', 20 , -6, 'Simon')
+theoretical.calc = this.price
+theoretical.publish()
+
+
