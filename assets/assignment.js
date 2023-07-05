22c7a50813b125b4a195c83e2ec36285486d58e2
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-05 11:30:55 -0400

    cleaned code

--- assets/assignment.js
+++ assets/assignment.js
@@ -1,27 +1,29 @@
 class Course {
+    #title;
+    #price;
 
     constructor(title, length, price) {
-        this.title = title;
+        this.#title = title;
         this.length = length;
-        this.price = price
+        this.#price = price
     }
 
     get calc(){
-        const sum = this.length / this.price
+        const sum = this.length / this.#price
         return `That class has ${sum} credits and will take ${this.length} days.`
     }
 
     set calc(val){
-        if(this.price < 0) {
+        if(this.#price < 0) {
             console.log('Bertha wnats to help!')
-            console.log(`You don't have enough to take ${this.title} this semester` )
-            this.price = 5            
+            console.log(`You don't have enough to take ${this.#title} this semester` )
+            this.#price = 5            
             return
         }
     }
     
     summary() {
-        return `You choose ${this.title}. It last ${this.length} days and cost \$${this.price} Simon.`
+        return `You choose ${this.#title}. It last ${this.length} days and cost \$${this.price} Simon.`
     }
 }
 
@@ -40,23 +42,29 @@ const offerings = {
 }
 
 class PracticalCourse extends Course {
+    #title;
     constructor(title, length, price, numOfExercises, name) {
         super(title, length, price)
+        this.#title = title
         this.numOfExercises = numOfExercises
         this.name = name
     }
     render(){
-        console.log(`${this.name}, ${this.title} has ${this.numOfExercises} assignments. ${this.calc}`)
+        console.log(`${this.name}, ${this.#title} has ${this.numOfExercises} assignments. ${this.calc}`)
     }
 }
 
 class TheoreticalCourse extends Course {
+    #title;
+  
     constructor(title, length, price, name) {
         super(title, length, price,)
+        this.#title = title;
+        this.price = price;
         this.name = name
     }
     publish() {
-        console.log(`${this.name}, Booker T Washington said ${this.title} is the way forward. ${this.calc}`)
+        console.log(`${this.name}, Booker T Washington said ${this.#title} is the way forward. ${this.calc}`)
     }
 }
 
@@ -65,6 +73,6 @@ const practical = new PracticalCourse('English',40, 2, 30, 'Bertha')
 practical.calc = this.price
 practical.render()
 
-const theoretical = new TheoreticalCourse('Agriculture', 20 , 5, 'Simon')
+const theoretical = new TheoreticalCourse('Agriculture', 20 ,-5, 'Simon')
 theoretical.calc = this.price
 theoretical.publish()
